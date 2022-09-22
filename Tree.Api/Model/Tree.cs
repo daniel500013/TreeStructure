696e84b0a7f73fbd02f989e0d73e4287375ad492
daniel500013 <30683843+daniel500013@users.noreply.github.com>
2022-09-23 00:30:48 +0200

    Created entity

---
+++ Tree.Api/Model/Tree.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Tree.Api.Model
+{
+    public class Tree
+    {
+        [Key]
+        public int TreeID { get; set; }
+        public string Name { get; set; }
+
+        public int? ParentID { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey(nameof(ParentID))]
+        public virtual Tree? TreeProp { get; set; }
+    }
+}
--- Tree.Api/TreeDbContext.cs
+++ Tree.Api/TreeDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Tree.Api
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tree.Api
 {
     public class TreeDbContext : DbContext
     {
+        public TreeDbContext(DbContextOptions<TreeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Model.Tree> Tree { get; set; }
     }
 }
