13213e911f3861e94b18f626e0e718e585e32e98
daniel500013 <30683843+daniel500013@users.noreply.github.com>
2022-09-23 00:36:11 +0200

    Complete get,add,change,remove functionality

--- Tree.Api/Program.cs
+++ Tree.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Tree.Api;
+using Tree.Api.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddDbContext<TreeDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString")));
 
 // Add services to the container.
+builder.Services.AddScoped<TreeService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Tree.Api/ViewModel/TreeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tree.Api.ViewModel
+{
+    public class TreeViewModel
+    {
+        public int TreeID { get; set; }
+        public string Name { get; set; }
+    }
+}
